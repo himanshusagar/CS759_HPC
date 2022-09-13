5d92e5d45c17cee2377c318a57f4d92bf973e9ae
hsagar2 <hsagar2@wisc.edu>
2022-09-13 13:54:03 -0500

    HW 1

--- HW01/task4.sh
+++ HW01/task4.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
 
+#SBATCH -p wacc
+#SBATCH -c 2
+#SBATCH -J FirstSlurm
+#SBATCH -o FirstSlurm.out -e FirstSlurm.err
+
+echo `hostname`
--- HW01/task6.cpp
+++ HW01/task6.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <chrono>
 
+int main() {
+    int N;
+    std::cin>>N;
+    for(int i = 0 ; i <= N ; i++)
+    {
+        printf("%d ", i);
+    }
+    printf("\n");
+    for(int i = N ; i >= 0 ; i--)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
