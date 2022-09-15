46493f7446461ff353c48735f5a913ac8ac26707
hsagar2 <hsagar2@wisc.edu>
2022-09-15 15:47:13 -0500

    HW 1

--- HW01/task6.cpp
+++ HW01/task6.cpp
@@ -5,10 +5,15 @@ int main(int argc, char *argv[])
 {
     if(argc != 2)
     {
-        std::cout << "Usage task6.cpp <Some Positive Integer>" << std::endl;
+        std::cout << "Usage ./task6 <Some Positive Integer>" << std::endl;
         return 0;
     }
     int N = std::atoi(argv[1]);
+    if(N < 0)
+    {
+        std::cout << "Usage ./task6 <Some Positive Integer>" << std::endl;
+        return 0;
+    }
     for(int i = 0 ; i <= N ; i++)
     {
         printf("%d ", i);
