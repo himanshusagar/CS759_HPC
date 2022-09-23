984724ca22b9bf56e42cc2560d149dd9dc2d5c55
Himanshu Sagar <himanshusagar@protonmail.com>
2022-09-23 15:26:39 -0500

    Adding first cuda code

---
+++ HW03/task1.cu
@@ -0,0 +1,52 @@
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <iostream>
+
+__global__ void add(int *a, int *c)
+{
+    for( int i = 1 ; i <= a[blockIdx.x]; i++)
+    {
+        c[blockIdx.x] = c[blockIdx.x] * (i);
+    }
+    std::printf("%d!=%d" , a[blockIdx.x] , c[blockIdx.x]);
+}
+
+int main(void)
+{
+    int N = 8;
+    int *a,  *c;       // host copies of a, b, c
+    int *d_a,  *d_c; // device copies of a, b, c
+    int size = N * sizeof(int);
+    cudaError_t cudaStatus;
+    // Alloc space for device copies of a, b, c
+    cudaMalloc((void **)&d_a, size);
+
+    cudaMalloc((void **)&d_c, size);
+    // Alloc space for host copies of a, b, c and setup input values
+    a = (int *)malloc(size);
+    for(int i = 0; i < N ; i++)
+    {
+        a[i] = i+1;
+    }
+    c = (int *)malloc(size);
+
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    // Launch add() kernel on GPU with N blocks
+    add<<<N, 1>>>(d_a, d_c);
+
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        return 0;
+    }
+
+    // Copy result back to host
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+    // Cleanup
+    free(a);
+    free(c);
+    cudaFree(d_a);
+    cudaFree(d_c);
+    return 0;
+}
