a848ff04b76dc767436eba09f2f7f4f2969d38c1
hsagar2 <hsagar2@wisc.edu>
2022-10-27 23:56:36 -0500

    Task 3 Done

--- HW07/task3.cpp
+++ HW07/task3.cpp
@@ -11,20 +11,24 @@ int main(int argc, char *argv[])
     std::printf("Number of threads: %d\n", omp_get_max_threads());
 
 #pragma omp parallel
-{
+    {
     //Get thread count
     int myId = omp_get_thread_num();
     //Print thread id.
     std::printf("I am thread No: %d\n", myId );
-    fflush(stdout); 
-    int sol = 1;
+    }
+//Parallel For for distributing tasks among 4 threads.
+#pragma omp parallel for
     for(int i = 1 ; i <= 8; i++)
     {
         //Generate factorial
-        sol *= i; 
+        int sol = 1;
+        for(int j = 1 ; j <= i; j++)
+        {
+            sol *= j;
+        }
         std::printf("%d!=%d\n", i, sol);
         fflush(stdout);
     }
-} // END #pragma omp parallel
     return 0;
 }
